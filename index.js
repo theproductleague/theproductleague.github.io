e16ed142e15fb111ff93353a223f319ee4db3121
Sanatan-Shrivastava <2018kucp1096@iiitkota.ac.in>
2023-08-06 15:51:28 -0500

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 AOS.init();
+
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
